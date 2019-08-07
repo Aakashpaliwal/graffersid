fed72cf93a3cc66a733fa2f76b537f8424f58192
aakash404 <saketpaliwal00@gmail.com>
2019-08-07 17:45:31 +0530

    table sorting , search and time functionality implemennted

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
-import axios from "axios";
 class Sidebar extends Component {
   render() {
     return (
